bd440eb36757ac77ffde6352bec1f3de37f9bdf6
Владимир <51334865+wingofnight@users.noreply.github.com>
2020-04-04 14:19:48 +0300

    Add files via upload

---
+++ ConsoleApplication77.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include "Classes.h"
+using namespace std;
+int whoBig(capital zero, capital one, capital two);
+
+int main()
+{
+	capital Mosscow;
+	capital Voronez;
+	capital Saratov;
+
+	Mosscow.capitel_strenght = 10;
+	Voronez.capitel_strenght = 1;
+	Saratov.capital_coordinate = -1;
+
+
+}
+
+int whoBig(capital zero, capital one, capital two) {
+	capital Capital[3]{ zero,one,two };
+	for (int i = 0; i < 3; i++)
+	{
+
+	}
+}
