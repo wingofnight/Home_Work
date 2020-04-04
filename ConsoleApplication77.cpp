33e78a8941c9562b801694cd1c14f45a2fd30248
Владимир <51334865+wingofnight@users.noreply.github.com>
2020-04-04 14:36:07 +0300

    Update ConsoleApplication77.cpp

--- ConsoleApplication77.cpp
+++ ConsoleApplication77.cpp
@@ -13,18 +13,22 @@ int main()
 	Mosscow.capitel_strenght = 10;
 	Voronez.capitel_strenght = 1;
 	Saratov.capital_coordinate = -1;
-	whoBig(Mosscow, Voronez, Saratov);
-
+	//whoBig(Mosscow, Voronez, Saratov);
+	cout << " have a many piople = " << whoBig(Mosscow, Voronez, Saratov);
 }
 
 int whoBig(capital zero, capital one, capital two) {
 	capital Capital[3]{ zero,one,two };
 	int buff = Capital[0].capitel_strenght;
+	string bigCapital = Capital[0].capital_name;
 	for (int i = 0; i < 3; i++)
 	{	
 		if (buff < Capital[i].capitel_strenght)
 		{
 			buff = Capital[i].capitel_strenght;
+			bigCapital = Capital[i].capital_name;
 		}
 	}
+	cout << bigCapital;
+	return buff;
 }
